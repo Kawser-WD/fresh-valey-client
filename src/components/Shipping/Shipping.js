9a64699fdf72a049fccde1b6a75727683c95e97d
Kawser365 <kawser07.khk@gmail.com>
2022-07-19 11:59:19 +0600

    update

--- src/components/Shipping/Shipping.js
+++ src/components/Shipping/Shipping.js
@@ -31,7 +31,6 @@ const Shipping = () => {
                 if (result) {
                     reset();
                     clearTheCart();
-                    navigate('/')
                    
                 }
             })
